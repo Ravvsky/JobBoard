1b7e5a020484c4919f00a8a529964903fb83e3fb
Ravvsky <pawel.rawsk@gmail.com>
2023-05-04 12:58:14 +0200

    feat: create RoundedImage component

--- src/app/Components/atoms/RoundedImage/RoundedImage.tsx
+++ src/app/Components/atoms/RoundedImage/RoundedImage.tsx
@@ -1,22 +1,14 @@
 import Image from "next/image";
 import type { ImageProps } from "next/image";
 
-const RoundedImage = ({
-  src,
-  alt,
-  width,
-  height,
-  className,
-  onClick,
-}: ImageProps) => {
+const RoundedImage = ({ src, alt, width, height }: ImageProps) => {
   return (
     <Image
       src={src}
       alt={alt}
       width={width}
       height={height}
-      className={`h-[4rem] max-h-[4rem] w-[4rem] max-w-[4rem] rounded-[55rem] bg-main-blue object-contain p-[0.5rem] ${className}`}
-      onClick={onClick}
+      className="max-h-[4rem] max-w-[4rem] rounded-[55rem] bg-main-blue object-contain p-[0.5rem]"
     />
   );
 };
