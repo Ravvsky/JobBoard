a9339ca4290d12c7bb624f24eedc32bf3c1fd2e0
Ravvsky <pawel.rawsk@gmail.com>
2023-05-10 13:40:04 +0200

    feat: modify Checkbox atom to inherit input types

--- src/app/Components/atoms/Checkbox/Checbox.tsx
+++ src/app/Components/atoms/Checkbox/Checbox.tsx
@@ -1,19 +1,17 @@
-const Checkbox = ({
-  width,
-  height,
-  id,
-  value,
-}: {
-  width: number;
-  height: number;
+import { InputHTMLAttributes } from "react";
+
+interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
+  width?: number;
+  height?: number;
   id: string;
   value: string;
-}) => {
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+}
+const Checkbox = ({ width = 1, height = 1, ...props }: CheckboxProps) => {
   return (
     <input
       type="checkbox"
-      id={id}
-      value={value}
+      {...props}
       className={`peer h-[${height}rem] w-[${width}rem] rounded-[0.4rem] border-[1px] border-solid border-main-blue border-opacity-40	bg-light-gray checked:bg-main-blue focus:ring-transparent  focus:ring-offset-0	checked:focus:bg-main-blue`}
     />
   );
