768b7b5edbf5832984207617f0fa4842a5ed8a98
Ravvsky <pawel.rawsk@gmail.com>
2023-05-10 23:05:41 +0200

    feat: add dispatch and category props to FilterButton

--- src/app/Components/molecules/FilterButton/FilterButton.tsx
+++ src/app/Components/molecules/FilterButton/FilterButton.tsx
@@ -1,22 +1,30 @@
 "use client";
 
+import { setFilter } from "@/redux/features/jobOfferFilters";
+import { useAppDispatch } from "@/redux/hooks";
 import { useState } from "react";
 
 const FilterButton = ({
   children,
   type,
   className,
+  category,
 }: {
   children: React.ReactNode;
   type: "button" | "submit" | "reset";
   className?: string;
+  category: string;
 }) => {
   const [isClicked, setIsClicked] = useState(false);
+  const dispatch = useAppDispatch();
+
   const clickHandler = () => {
     setIsClicked(!isClicked);
+    dispatch(setFilter({ category, value: children as string }));
   };
   return (
     <button
+      data-cy="FilterButton"
       type={type}
       onClick={clickHandler}
       className={` rounded-[5rem] ${
