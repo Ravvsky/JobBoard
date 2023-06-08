7b65a66a376c4573034c1d3af9d900f8f56b0823
Ravvsky <pawel.rawsk@gmail.com>
2023-06-08 20:03:24 +0200

    feat: update Checkbox component to work with and without Formik

--- src/app/Components/atoms/Checkbox/Checbox.tsx
+++ src/app/Components/atoms/Checkbox/Checbox.tsx
@@ -1,18 +1,30 @@
-import { InputHTMLAttributes } from "react";
+"use client";
+import { FieldHookConfig } from "formik";
+import { InputHTMLAttributes, Ref } from "react";
 
-interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
-  width?: number;
-  height?: number;
-  id: string;
-  value: string;
-  onChange: React.ChangeEventHandler<HTMLInputElement>;
-}
-const Checkbox = ({ width = 1, height = 1, ...props }: CheckboxProps) => {
+type CheckboxProps = InputHTMLAttributes<HTMLInputElement> &
+  FieldHookConfig<boolean> & {
+    width?: number;
+    height?: number;
+    id: string;
+    field?: any;
+    ref?: Ref<HTMLInputElement>;
+  };
+
+const Checkbox = ({
+  width = 1,
+  height = 1,
+  id,
+  field,
+  ...props
+}: CheckboxProps) => {
   return (
     <input
       type="checkbox"
+      id={id}
+      {...field}
       {...props}
-      className={`peer h-[${height}rem] w-[${width}rem] rounded-[0.4rem] border-[1px] border-solid border-main-blue border-opacity-40	bg-light-gray checked:bg-main-blue focus:ring-transparent  focus:ring-offset-0	checked:focus:bg-main-blue`}
+      className={`peer h-[${height}rem] w-[${width}rem] cursor-pointer rounded-[0.4rem] border-[1px] border-solid border-main-blue border-opacity-40 bg-light-gray checked:bg-main-blue checked:hover:bg-main-blue focus:ring-transparent focus:ring-offset-0 checked:focus:bg-main-blue`}
     />
   );
 };
