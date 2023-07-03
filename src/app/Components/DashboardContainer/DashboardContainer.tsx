80fa43f032d4bc2528e55926abc7d5fd55306b9f
Ravvsky <pawel.rawsk@gmail.com>
2023-07-04 01:45:20 +0200

    fix: fix DashboardContainer styling

--- src/app/Components/DashboardContainer/DashboardContainer.tsx
+++ src/app/Components/DashboardContainer/DashboardContainer.tsx
@@ -6,8 +6,8 @@ const DashboardContainer = ({
   children: React.ReactNode;
 }) => {
   return (
-    <div className="min-h-[5rem] w-full rounded-[1.5rem] bg-light-gray">
-      <div className="p-[2.5rem] text-[2rem] font-semibold">{title}</div>
+    <div className="min-h-[5rem] w-full rounded-[1.5rem] bg-light-gray p-[2.5rem]">
+      <div className=" pb-[1.5rem] text-[2rem] font-semibold">{title}</div>
       {children}
     </div>
   );
