110e8027bbf8b13ea586f889ca5e5cfcdc7ad6d4
Ravvsky <pawel.rawsk@gmail.com>
2023-07-04 18:28:32 +0200

    feat: add className prop to DashboardContainer

--- src/app/Components/DashboardContainer/DashboardContainer.tsx
+++ src/app/Components/DashboardContainer/DashboardContainer.tsx
@@ -1,12 +1,22 @@
+import { twMerge } from "tailwind-merge";
+
 const DashboardContainer = ({
   title,
+  className,
   children,
 }: {
   title: string;
+  className?: string;
   children: React.ReactNode;
 }) => {
   return (
-    <div className="min-h-[5rem] w-full rounded-[1.5rem] bg-light-gray p-[2.5rem]">
+    <div
+      className={twMerge(
+        `min-h-[5rem] w-full rounded-[1.5rem] bg-light-gray p-[2.5rem] ${
+          className && className
+        }`,
+      )}
+    >
       <div className=" pb-[1.5rem] text-[2rem] font-semibold">{title}</div>
       {children}
     </div>
