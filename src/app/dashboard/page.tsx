65c8b15bc80207db86bada2bc3d8d2f8b4ce64fe
Ravvsky <pawel.rawsk@gmail.com>
2023-08-05 21:51:11 +0200

    feat: create dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,24 @@
-import TestCompo from "../Components/TestCompo/TestCompo";
+import DashboardContainer from "../Components/DashboardContainer/DashboardContainer";
+import { getUserData } from "../_actions";
+import JobTimeline from "../Components/JobTimeline/JobTimeline";
+import UserBasicInfo from "../Components/UserBasicInfo/UserBasicInfo";
 
-const Page = () => {
+const Page = async () => {
+  const data = await getUserData();
+
   return (
     <div>
-      <TestCompo />
+      <UserBasicInfo user={data} />
+      <div className="mb-[10rem] mt-[22rem] flex gap-[2rem]">
+        <div className="flex-1 grow">
+          <JobTimeline initialJobs={data.jobTimeline} />
+        </div>
+        <div className="flex-1 grow">
+          <DashboardContainer title="My current skills">
+            <div></div>
+          </DashboardContainer>
+        </div>
+      </div>
     </div>
   );
 };
