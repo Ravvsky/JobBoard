3bd7b06c00e29e66e79bd57c6bf7ef8394a28eb3
Ravvsky <pawel.rawsk@gmail.com>
2023-09-24 15:38:51 +0200

    chore: add twMerge to RoundedImage

--- src/app/Components/atoms/RoundedImage/RoundedImage.tsx
+++ src/app/Components/atoms/RoundedImage/RoundedImage.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import type { ImageProps } from "next/image";
+import { twMerge } from "tailwind-merge";
 
 const RoundedImage = ({
   src,
@@ -15,7 +16,9 @@ const RoundedImage = ({
       alt={alt}
       width={width}
       height={height}
-      className={`max-h-[4rem] max-w-[4rem] rounded-[55rem] bg-main-blue object-contain p-[0.5rem] ${className}`}
+      className={twMerge(
+        `max-h-[4rem] max-w-[4rem] rounded-[55rem] bg-main-blue object-contain p-[0.5rem] ${className}`,
+      )}
       onClick={onClick}
     />
   );
