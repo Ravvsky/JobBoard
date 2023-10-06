16f9ff879a994828749a7feafb95acd26fcced27
Ravvsky <pawel.rawsk@gmail.com>
2023-10-06 22:16:40 +0200

    test

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg")
+      rule.test?.test?.(".svg"),
     );
 
     config.module.rules.push(
@@ -18,7 +18,7 @@ module.exports = {
         issuer: /\.[jt]sx?$/,
         resourceQuery: { not: /url/ }, // exclude if *.svg?url
         use: ["@svgr/webpack"],
-      }
+      },
     );
 
     // Modify the file loader rule to ignore *.svg, since we have it handled now.
@@ -39,5 +39,12 @@ module.exports = {
   experimental: {
     appDir: true,
     serverActions: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
   },
 };
